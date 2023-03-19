b9e5e39858572b2a511ff538138bc441673daed3
alvinkigondu <kigondualvin@gmail.com>
2023-03-19 14:29:18 -0700

    Printing combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,30 +4,39 @@
 /**
  * main - this is the main function
  *
- * Return: 0 when successfull
+ * Return: 0 when success
+ *
  */
-
-int main(void)
+    int main  (void)
 {
-	int i = 0;
-	int j = 0;
+    
+	    int i = '0';
+	    int j = '0';
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 1; j < 10; j++)
-		{
-			putchar(48 + i);
-			putchar(48 + j);
-			putchar(' ');
-			if (i == 8 && j == 9)
-			{
-				exit(0);
-			} else
-			{
-				putchar(',');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+	   while (i <= '9')
+	   {
+	   
+		   while (j <= '9')
+		   {
+		   
+			   if (!(i > j) || i == j)
+			   {
+			       putchar(i);
+			       putchar(j);
+			       if (i == '8' && j == '9')
+			       {
+			             putchar('\n');
+			       }
+			       else 
+			       {
+			             putchar(',');
+				     putchar(' ');
+    			       }
+		   }
+		 j++;   
+	  }
+          j = '0';
+          i++;
+       }
+return (0);
+ } 
