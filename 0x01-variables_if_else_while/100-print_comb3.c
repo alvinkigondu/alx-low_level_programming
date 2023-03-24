de9b78ea01a1e213535e5f3b24a04e73454e2471
alvinkigondu <kigondualvin@gmail.com>
2023-03-23 17:50:11 -0700

    Questions for today

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -15,28 +14,30 @@ int main(void)
 
 	   while (i <= '9')
 	   {
-	   
+	      
 		   while (j <= '9')
 		   {
 		   
 			   if (!(i > j) || i == j)
 			   {
-			       putchar(i);
-			       putchar(j);
-			       if (i == '8' && j == '9')
-			       {
-			             putchar('\n');
-			       }
-			       else 
-			       {
-			             putchar(',');
-				     putchar(' ');
-    			       }
-		            }
-		            j++;   
-	             }
+			             putchar(i);
+			             putchar(j);
+			             if (i == '8' && j == '9')
+				     {
+					     putchar('\n');
+			             }
+			            else 
+			             {
+					      putchar(',');
+                                              putchar(' ');
+				     }
+		           
+		          }
+		          j++;   
+	            
+		   }
 		    j = '0';
                     i++;
-           }
+	   }
 return (0);
- } 
+} 
