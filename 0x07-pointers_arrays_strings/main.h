8e86f9b078d54a0b12faee3ac026732d7b444cc2
alvinkigondu <kigondualvin@gmail.com>
2023-04-03 16:23:26 -0700

    function that sets the value of a pointer to a char.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,8 +4,9 @@
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
+char *_istrchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
+char *i_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 
 #endif
