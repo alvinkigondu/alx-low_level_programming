3214ca0a96b4004ab95500e88519d8f05c3e9cda
alvinkigondu <kigondualvin@gmail.com>
2023-04-03 14:18:37 -0700

    function that copies files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,20 +2,20 @@
 
 /**
  * _memset - test function
- * fills memorywith constant byte
- * @s: pointed destination
- * @b:constant byte
+ * Fills memory with constant byte
+ * @S: pointed destination
+ * @b: constant type
  * @n: byte
  * Return: 0
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; i < n; i++)
-        {
-                s[i] = b;
-        }
-        return (s);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
