07224142630966a54acdd3b5691ab4e2f2d14870
alvinkigondu <kigondualvin@gmail.com>
2023-04-03 13:25:35 -0700

    function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,20 @@
 
 /**
  * _memset - test function
- * fills memory with constant byte
- *@s: pointed destination
- *@b: constant byte
- *@n: byte
- *Return: Always 0 (Success)
+ * fills memorywith constant byte
+ * @s: pointed destination
+ * @b:constant byte
+ * @n: byte
+ * Return: 0
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+        unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
+        for (i = 0; i < n; i++)
+        {
+                s[i] = b;
+        }
+        return (s);
 }
