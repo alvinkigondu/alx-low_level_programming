5b6205469181e565060b2ba241f5e2150db718b8
alvinkigondu <kigondualvin@gmail.com>
2023-04-09 05:59:32 -0700

    Create the static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd>
+
+/**
+ * _putchar - writes a character c to stdout
+ * @c - character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
